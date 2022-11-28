270cf55c8c07d6cfdc971246fe2f9b9913cb2b8f
AbNashaddhi <117175615+AbNashaddhi@users.noreply.github.com>
2022-11-28 19:42:29 +0530

    Add files via upload

---
+++ VisitorPattern/VisitorPattern/IVisitor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    public interface IVisitor
+    {
+        void Accept(Mobile andriod);
+        void Accept(Laptop laptop);
+    }
+
+    public class Mobile : IStore
+    {
+        public string MobileName { get; set; }
+        public decimal Price { get; set; }
+        public string ModelName { get; set; }
+
+        public void Visit(IVisitor visitor)
+        {
+            visitor.Accept(this);
+        }
+    }
+    public class Laptop : IStore
+    {
+        public string LaptopName { get; set; }
+        public decimal Price { get; set; }
+        public string ModelName { get; set; }
+
+        public void Visit(IVisitor visitor)
+        {
+            visitor.Accept(this);
+        }
+    }
+    public class PriceVisitor : IVisitor
+    {
+        private const int Mobile_Discount = 5;
+        private const int Laptop_Discount = 2;
+
+        public void Accept(Mobile mobile)
+        {
+            decimal mobilePriceAfterDiscount = mobile.Price - ((mobile.Price / 100) * Mobile_Discount);
+            Console.WriteLine("Mobile {0} price: {1}", mobile.MobileName, mobilePriceAfterDiscount);
+
+        }
+
+        public void Accept(Laptop laptop)
+        {
+            decimal laptopPriceAfterDiscount = laptop.Price - ((laptop.Price / 100) * Mobile_Discount);
+            Console.WriteLine("Mobile {0} price: {1}", laptop.ModelName, laptopPriceAfterDiscount);
+
+        }
+
+    }
+}
---
+++ VisitorPattern/VisitorPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IStore> store = new List<IStore>
+        {
+            new Mobile() { MobileName = "A1", Price = 200M},
+            new Mobile() { MobileName = "A2", Price = 100M},
+
+            new Laptop() { LaptopName = "B1", Price = 50M, },
+            new Laptop() { LaptopName = "B2", Price = 30M, }
+        };
+
+            PriceVisitor priceVisitor = new PriceVisitor();
+            foreach (var element in store)
+            {
+                element.Visit(priceVisitor);
+            }
+           
+            Console.ReadLine();
+
+        }
+    }
+}
